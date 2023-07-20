d64fc875615562b9f565038855cf6c3eff2f8318
Kashif Khan <KashifKhnx04@gmail.com>
2023-07-20 18:46:09 +0500

    adding new Page

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,19 @@
+// for the mobile navigation or humburger menu
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
     primaryNav.toggleAttribute("data-visible");
     primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", true) : navToggle.setAttribute("aria-expanded", false);
+});
+
+// for the active page link
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll(".primary-navigation a");
+
+navLinks.forEach(link => {
+    if(link.href == window.location.origin + activePage)
+        link.classList.add('active');
 });
 
 
